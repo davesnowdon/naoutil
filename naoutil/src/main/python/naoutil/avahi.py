ce3d26697eea8acb080d5fc94d7b94087bc4dd76
Axel Voitier <axel.voitier@gmail.com>
2013-04-07 00:52:07 +0200

    Change the way the avahi detection loop is handled. Also, add capability to tell if a naoqi is local or not (ie. running on the machine we are on).

--- naoutil/src/main/python/naoutil/avahi.py
+++ naoutil/src/main/python/naoutil/avahi.py
@@ -16,7 +16,7 @@ from dbus.mainloop.glib import DBusGMainLoop
 # http://cgit.freedesktop.org/dbus/dbus-python/tree/examples
 # http://www.no-ack.org/2010/07/writing-simple-dbus-client-in-python.html
 
-# Constant definition coming from the python-avahi module.
+# Constant definitions coming from the python-avahi module.
 # The python-avahi module is not installed by default on Nao or usual OSs.
 # But it is mainly made of constant definitions. So I replicate here the important ones.
 AVAHI_DBUS_NAME = 'org.freedesktop.Avahi'
@@ -24,27 +24,32 @@ AVAHI_DBUS_INTERFACE_SERVER = AVAHI_DBUS_NAME + '.Server'
 AVAHI_DBUS_INTERFACE_SERVICE_BROWSER = AVAHI_DBUS_NAME + '.ServiceBrowser'
 AVAHI_IF_UNSPEC = -1
 AVAHI_PROTO_UNSPEC, AVAHI_PROTO_INET, AVAHI_PROTO_INET6  = -1, 0, 1
+AVAHI_LOOKUP_RESULT_LOCAL = 8
 
-def service_resolved(*args):
+def _serviceResolvedCb(*args):
     global nbServicesFound, servicesFound, gloop
-    #print 'service resolved'
+    #print 'service resolved', args[5]
     labels = ['interface', 'protocol', 'name', 'type', 'domain', 'host', 'aprotocol', 'address', 'port', 'txt', 'flags']
     #servicesFound.append(dict(zip(labels, args)))
     servicesFound.append({
         'robot_name': str(args[labels.index('name')]),
         'host_name': str(args[labels.index('host')]),
         'ip_address': str(args[labels.index('address')]),
-        'naoqi_port': int(args[labels.index('port')])
+        'naoqi_port': int(args[labels.index('port')]),
+        'local': bool(args[labels.index('flags')] & AVAHI_LOOKUP_RESULT_LOCAL)
     })
-    if len(servicesFound) == nbServicesFound:
+    nbServicesFound -= 1
+    if nbServicesFound == 0:
         gloop.quit()
 
-def print_error(*args):
-    global gloop
-    print 'error', args
-    gloop.quit()
+def _doNothingErrorCb(*args):
+    global nbServicesFound, gloop
+    print args
+    nbServicesFound -= 1
+    if nbServicesFound == 0:
+        gloop.quit()
     
-def myhandler(interface, protocol, name, stype, domain, flags):
+def _itemNewCb(interface, protocol, name, stype, domain, flags):
     global server, nbServicesFound
     #print "Found service '%s' type '%s' domain '%s' " % (name, stype, domain)
     nbServicesFound += 1
@@ -55,7 +60,7 @@ def myhandler(interface, protocol, name, stype, domain, flags):
 
     server.ResolveService(interface, protocol, name, stype, 
         domain, AVAHI_PROTO_UNSPEC, dbus.UInt32(0), 
-        reply_handler=service_resolved, error_handler=print_error)
+        reply_handler=_serviceResolvedCb, error_handler=_doNothingErrorCb)
         
 
 nbServicesFound = 0
@@ -80,7 +85,7 @@ def findAllNAOs(ipv6=False):
                 protoInet, '_naoqi._tcp', 'local', dbus.UInt32(0))),
             AVAHI_DBUS_INTERFACE_SERVICE_BROWSER)
 
-    sbrowser.connect_to_signal("ItemNew", myhandler)
+    sbrowser.connect_to_signal("ItemNew", _itemNewCb)
 
     gloop = gobject.MainLoop()
     gloop.run()
