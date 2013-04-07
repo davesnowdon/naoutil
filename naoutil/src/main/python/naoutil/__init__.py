d8bd2cdf8894088b4704de8c928a8eaa975f4039
Axel Voitier <axel.voitier@gmail.com>
2013-04-07 04:34:33 +0200

    Make the ALModule wrapper capable of assigning a module name when none is provided.

--- naoutil/src/main/python/naoutil/__init__.py
+++ naoutil/src/main/python/naoutil/__init__.py
@@ -1,5 +1,6 @@
 import __main__
 from naoqi import ALModule as _ALModule
+from naoutil.general import object_to_FQCN
 
 class ALModule(_ALModule):
     '''
@@ -8,9 +9,15 @@ class ALModule(_ALModule):
     So you don't have to care about the rule 
     "The name given in the constructor must be the same as the variable name, which must be global".
     
+    Also, you can just not supply any module name. One based on the fully qualified class name will be assigned to your module.
+    If you need it later on, use self.moduleName.
+    
     Just instanciate your module the way you want!
     '''
-    def __init__(self, moduleName):
-        moduleName = str(moduleName)
-        _ALModule.__init__(self, moduleName)
-        setattr(__main__, moduleName, self)
+    def __init__(self, moduleName=None):
+        if moduleName is None:
+            moduleName = object_to_FQCN(self).replace('.', '_').lstrip('_')
+        self.moduleName = str(moduleName)
+        print self.moduleName
+        _ALModule.__init__(self, self.moduleName)
+        setattr(__main__, self.moduleName, self)
