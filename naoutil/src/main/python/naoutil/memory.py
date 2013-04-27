f78ddacd7acd4663fadb44f9bc16282792e774ac
Axel Voitier <axel.voitier@gmail.com>
2013-04-28 01:09:06 +0200

    Adding methods for subscribing to events.

--- naoutil/src/main/python/naoutil/memory.py
+++ naoutil/src/main/python/naoutil/memory.py
@@ -21,19 +21,38 @@ class _SubscriberModule(ALModule):
     def __init__(self):
         ALModule.__init__(self)
         self.memory = ALProxy('ALMemory')
-        self.dataNameToCallback = {}
+        self.dataNameToMicroEventCallback = {}
+        self.dataNameToEventCallback = {}
+        
+    def subscribeToEvent(self, dataName, callback):
+        self.dataNameToEventCallback[dataName] = callback
+        self.memory.subscribeToEvent(dataName, self.moduleName, 'eventCB')
+        
+    def unsubscribeToEvent(self, dataName):
+        if dataName in self.dataNameToEventCallback:
+            self.memory.unsubscribeToEvent(dataName, self.moduleName)
+            del self.dataNameToEventCallback[dataName]
+        
+    def eventCB(self, dataName, value, message):
+        self.dataNameToEventCallback[dataName](dataName, value, message)
         
     def subscribeToMicroEvent(self, dataName, callback, cbMessage):
-        self.dataNameToCallback[dataName] = callback
+        self.dataNameToMicroEventCallback[dataName] = callback
         self.memory.subscribeToMicroEvent(dataName, self.moduleName, cbMessage, 'microEventCB')
         
     def unsubscribeToMicroEvent(self, dataName):
-        if dataName in self.dataNameToCallback:
+        if dataName in self.dataNameToMicroEventCallback:
             self.memory.unsubscribeToMicroEvent(dataName, self.moduleName)
-            del self.dataNameToCallback[dataName]
+            del self.dataNameToMicroEventCallback[dataName]
         
     def microEventCB(self, dataName, value, message):
-        self.dataNameToCallback[dataName](dataName, value, message)
+        self.dataNameToMicroEventCallback[dataName](dataName, value, message)
+        
+def subscribeToEvent(dataName, callback):
+    _SubscriberModule().subscribeToEvent(dataName, callback)
+        
+def unsubscribeToEvent(dataName):
+    _SubscriberModule().unsubscribeToEvent(dataName)
         
 def subscribeToMicroEvent(dataName, callback, cbMessage=''):
     _SubscriberModule().subscribeToMicroEvent(dataName, callback, cbMessage)
