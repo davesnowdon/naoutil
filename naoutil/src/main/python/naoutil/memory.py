43f177bfae8279eb733f637b73cd6a74dcd0ddd8
Axel Voitier <axel.voitier@gmail.com>
2013-09-01 00:00:55 +0200

    Update naoutil.memory with the latest change. And fix the wrong PEP-8 related changed that broke access to NaoQi ALMemory module which is not PEP-8 compliant... Added non PEP-8 synonyms of the (un)subscribe_to_* functions of naoutil.memory to avoid breaking compatibility and keep the same name than the ALMemory module for easier migration of someone code.

--- naoutil/src/main/python/naoutil/memory.py
+++ naoutil/src/main/python/naoutil/memory.py
@@ -8,39 +8,21 @@ Provide helper functions to easilly subscribe to ALMemory
 events and micro events.
 '''
 
-import weakref
-
 from naoqi import ALProxy
 
-from naoutil import ALModule
-
-def _singleton(cls):
-    '''
-    Decorator for a class to transform it as a singleton.
-    '''
-    instances = weakref.WeakValueDictionary()
-    def getinstance():
-        '''
-        Lookup for the class in the weak dict and return its singleton instance.
-        Called when creating an object.
-        '''
-        try:
-            return instances[cls]
-        except KeyError:
-            instances[cls] = cls()
-            return instances[cls]
-    return getinstance
+from naoutil.module import Module
+from naoutil.general import singleton
 
 
-@_singleton
-class _SubscriberModule(ALModule):
+@singleton
+class _SubscriberModule(Module):
     '''
     Singleton ALModule used to subscribe to events and micro-events.
     Not made to be used by a user.
     Prefer to call the module functions.
     '''
     def __init__(self):
-        ALModule.__init__(self)
+        Module.__init__(self)
         self.memory = ALProxy('ALMemory')
         self.data_name_to_micro_event_cb = {}
         self.data_name_to_event_cb = {}
@@ -52,7 +34,7 @@ class _SubscriberModule(ALModule):
         Keep trace of the (data_name, callback) association.
         '''
         self.data_name_to_event_cb[data_name] = callback
-        self.memory.subscribe_to_event(data_name, self.module_name, '_event_cb')
+        self.memory.subscribeToEvent(data_name, self.module_name, '_event_cb')
         
     def unsubscribe_to_event(self, data_name):
         '''
@@ -60,7 +42,7 @@ class _SubscriberModule(ALModule):
         Remove reference to the (data_name, callback) association.
         '''
         if data_name in self.data_name_to_event_cb:
-            self.memory.unsubscribe_to_event(data_name, self.module_name)
+            self.memory.unsubscribeToEvent(data_name, self.module_name)
             del self.data_name_to_event_cb[data_name]
         
     def _event_cb(self, data_name, value, message):
@@ -78,7 +60,7 @@ class _SubscriberModule(ALModule):
         Keep trace of the (data_name, callback) association.
         '''
         self.data_name_to_micro_event_cb[data_name] = callback
-        self.memory.subscribe_to_micro_event(data_name, self.module_name,
+        self.memory.subscribeToMicroEvent(data_name, self.module_name,
                                              cb_message, '_micro_event_cb')
         
     def unsubscribe_to_micro_event(self, data_name):
@@ -87,7 +69,7 @@ class _SubscriberModule(ALModule):
         Remove reference to the (data_name, callback) association.
         '''
         if data_name in self.data_name_to_micro_event_cb:
-            self.memory.unsubscribe_to_micro_event(data_name, self.module_name)
+            self.memory.unsubscribeToMicroEvent(data_name, self.module_name)
             del self.data_name_to_micro_event_cb[data_name]
         
     def _micro_event_cb(self, data_name, value, message):
@@ -103,12 +85,14 @@ def subscribe_to_event(data_name, callback):
     Subscribe to an event.
     '''
     _SubscriberModule().subscribe_to_event(data_name, callback)
+subscribeToEvent = subscribe_to_event
         
 def unsubscribe_to_event(data_name):
     '''
     Unsubscribe to an event.
     '''
     _SubscriberModule().unsubscribe_to_event(data_name)
+unsubscribeToEvent = unsubscribe_to_event
         
 def subscribe_to_micro_event(data_name, callback, cb_message=''):
     '''
@@ -116,9 +100,11 @@ def subscribe_to_micro_event(data_name, callback, cb_message=''):
     '''
     _SubscriberModule().subscribe_to_micro_event(data_name, callback,
                                                  cb_message)
+subscribeToMicroEvent = subscribe_to_micro_event
         
 def unsubscribe_to_micro_event(data_name):
     '''
     Unsubscribe to an event.
     '''
     _SubscriberModule().unsubscribe_to_micro_event(data_name)
+unsubscribeToMicroEvent = unsubscribe_to_micro_event
